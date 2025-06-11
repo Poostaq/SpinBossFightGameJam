7ab73519204aacc2fa756919addee9962938357e
Poostaq <piootrek.nowicki@gmail.com>
2025-06-11 16:02:19 +0200

    fix codexes indentation problems

--- Features/Database.gd
+++ Features/Database.gd
@@ -15,17 +15,20 @@ func load_all_data():
 	load_player_deck()
 
 func load_cassettes():
-        var file = FileAccess.open("res://Data/cassettes_db.json", FileAccess.READ)
-        if file:
-                cassettes = JSON.parse_string(file.get_as_text())
-        else:
-                push_error("Failed to load cassettes_db.json")
+	var file = FileAccess.open("res://Data/cassettes_db.json", FileAccess.READ)
+	if file:
+			cassettes = JSON.parse_string(file.get_as_text())
+	else:
+			push_error("Failed to load cassettes_db.json")
+
+
 func load_status_effects():
 	var file = FileAccess.open("res://Data/status_effect.json", FileAccess.READ)
 	if file:
 		effects = JSON.parse_string(file.get_as_text())
 	else:
 		push_error("Failed to load status_effects.json")
+
 
 func load_boss_decks():
 	var file = FileAccess.open("res://Data/boss_decks.json", FileAccess.READ)
